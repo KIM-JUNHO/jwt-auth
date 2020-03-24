86f156e288f18ad7ff37b1c5d7258640790e5837
prpr48 <prpr48@naver.com>
2020-03-24 21:49:03 +0900

    basic apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,26 @@
 import 'reflect-metadata';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
 (async () => {
   const app = express();
   app.get('/', (_req, res) => res.send('hello'));
+
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+          hello: String!
+      }
+      `,
+    resolvers: {
+      Query: {
+        hello: () => 'hello world'
+      }
+    }
+  });
+
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log('express server started');
   });

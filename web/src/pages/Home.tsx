dac0869d9eda1838c62b022cffd39be923096b10
prpr48 <prpr48@naver.com>
2020-03-25 00:53:24 +0900

    add nav menu

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
-  return <div>index</div>;
+  return <div>Home page</div>;
 };

d2f6d2f3e06af1a67cae9cc17221f90ab8c1d07f
prpr48 <prpr48@naver.com>
2020-03-25 00:57:28 +0900

    basic input form submit

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {}
 
 export const Register: React.FC<Props> = () => {
-  return <div>register page</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        console.log('form submitted');
+        console.log(email, password);
+      }}
+    >
+      <div>
+        <input
+          value={email}
+          placeholder="email"
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit">register</button>
+    </form>
+  );
 };

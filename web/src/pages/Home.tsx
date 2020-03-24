e6bed1979a356692ad53f6ccdc35c8a59e2e00ae
prpr48 <prpr48@naver.com>
2020-03-25 01:05:27 +0900

    use register

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
+import { useRegisterMutation } from '../generated/graphql';
 
 interface Props {}
 
 export const Register: React.FC<Props> = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [register] = useRegisterMutation();
 
   return (
     <form
-      onSubmit={e => {
+      onSubmit={async e => {
         e.preventDefault();
         console.log('form submitted');
         console.log(email, password);
+        const response = await register({
+          variables: { email, password }
+        });
+        console.log(response);
       }}
     >
       <div>

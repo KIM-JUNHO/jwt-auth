336c8dff98f98a1a76786f0bbc1c16ef3afb9f72
prpr48 <prpr48@naver.com>
2020-03-25 01:50:01 +0900

    use useEffect accesstoken

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Routes } from './Routes';
 import { getAccessToken } from './accessToken';
+import { App } from './App';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -21,7 +21,7 @@ const client = new ApolloClient({
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Routes />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')

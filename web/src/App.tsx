0fc6a53da51c38a9bfcdd2d18b54e5769f705f05
prpr48 <prpr48@naver.com>
2020-03-25 00:18:34 +0900

    use gql with useQuery

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 function App() {
-  return <div>hello</div>;
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;

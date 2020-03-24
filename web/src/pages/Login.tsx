3a357bdeae9b65db63afd9fbc3b5f5e61f2a81fc
prpr48 <prpr48@naver.com>
2020-03-25 01:41:53 +0900

    add header authorization bearer token

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { useLoginMutation } from '../generated/graphql';
+import { setAccessToken } from '../accessToken';
 
 interface Props {}
 
@@ -20,6 +21,10 @@ export const Login: React.FC<RouteComponentProps> = ({ history }) => {
         });
 
         console.log(response);
+
+        if (response && response.data) {
+          setAccessToken(response.data.login.accessToken);
+        }
 
         history.push('/');
       }}
